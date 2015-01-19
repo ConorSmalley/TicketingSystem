da59d11c2e7c4dcf4c8aa88584528c8efe9b5a80
B2045995 <B2045995@5223-014D205300.hallam.shu.ac.uk>
2015-01-19 13:46:02 +0000

    Gateway

--- src/ticketing/system/TicketingSystem.java
+++ src/ticketing/system/TicketingSystem.java
@@ -15,6 +15,9 @@ public class TicketingSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
+        
         // TODO code application logic here
     }
     
