b19953ffff994c655f1aa37996ae2017dc72ca79
ConorSmalley <conorsmalley93@gmail.com>
2015-01-19 14:10:24 +0000

    Person Completed

--- src/ticketing/system/Person.java
+++ src/ticketing/system/Person.java
@@ -10,5 +10,64 @@ package ticketing.system;
  * @author Conor
  */
 public class Person {
-    
+
+    private String Name;
+    private String AddressLine1;
+    private String AddressLine2;
+    private String AddressLine3;
+    private String Town;
+    private String County;
+    private String Postcode;
+
+    Person(String Name) {
+        this.Name = Name;
+    }
+
+    public String GetAddressL1() {
+        return AddressLine1;
+    }
+
+    public String GetAddressL2() {
+        return AddressLine2;
+    }
+
+    public String GetAddressL3() {
+        return AddressLine3;
+    }
+
+    public String GetTown() {
+        return Town;
+    }
+
+    public String GetCounty() {
+        return County;
+    }
+
+    public String GetPostcode() {
+        return Postcode;
+    }
+
+    public void SetAddressL1(String AddressLine1) {
+        this.AddressLine1 = AddressLine1;
+    }
+
+    public void SetAddressL2(String AddressLine2) {
+        this.AddressLine2 = AddressLine2;
+    }
+
+    public void SetAddressL3(String AddressLine3) {
+        this.AddressLine3 = AddressLine3;
+    }
+
+    public void SetTown(String Town) {
+        this.Town = Town;
+    }
+
+    public void SetCounty(String County) {
+        this.County = County;
+    }
+
+    public void SetPostcode(String Postcode) {
+        this.Postcode = Postcode;
+    }
 }
