4c456bee94daedfce42433113582c71da28723a9
Curtis <curtisbail@gmail.com>
2015-01-19 13:36:43 +0000

    Employee class added

---
+++ src/ticketing/system/Employee.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ticketing.system;
+
+/**
+ *
+ * @author Curtis
+ */
+public class Employee {
+    private String name;
+    private int id;
+    
+    public Employee(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+}
