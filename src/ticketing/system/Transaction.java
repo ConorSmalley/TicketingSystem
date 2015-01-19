18502c0d331c940348b6e3e547e2920ec9467da5
ConorSmalley <conorsmalley93@gmail.com>
2015-01-19 14:28:43 +0000

    Token Complete, Transaction Completed, TransactionType added to provide enum, Transaction list started

--- src/ticketing/system/Transaction.java
+++ src/ticketing/system/Transaction.java
@@ -5,10 +5,25 @@
  */
 package ticketing.system;
 
+import java.util.Date;
+
 /**
  *
  * @author Conor
  */
 public class Transaction {
-    
+
+    private int Id;
+    private TransactionType type;
+    private double Amount;
+    private Date datePurchased;
+
+    Transaction(TransactionType type, double Amount, Date datePurchased) {
+        this.type = type;
+        this.Amount = Amount;
+        this.datePurchased = datePurchased;
+    }
+    public int getId(){
+        return Id;
+    }
 }
