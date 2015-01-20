79e905116a3f8723b4638b63f5dcd0d4e311da3c
ConorSmalley <conorsmalley93@gmail.com>
2015-01-20 00:13:10 +0000

    All of the classes are now added although are missing functions

---
+++ src/ticketing/system/TicketPassPool.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ticketing.system;
+
+import java.util.List;
+
+/**
+ *
+ * @author Conor
+ */
+public class TicketPassPool {
+    private List<Ticket> tickets;
+    private List<Pass> passes;
+}
