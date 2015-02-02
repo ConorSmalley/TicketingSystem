c5ab8fb9c5333a48de450a686403b3d964e2637a
Curtis <curtisbail@gmail.com>
2015-02-02 10:15:51 +0000

    TravelPoint implemented

--- src/ticketing/system/TravelPoint.java
+++ src/ticketing/system/TravelPoint.java
@@ -10,5 +10,28 @@ package ticketing.system;
  * @author Conor
  */
 class TravelPoint {
+    private int id;
+    private String name;
+    private String type;
+    private PaymentHub paymenthub;
     
+    public TravelPoint(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getType(){
+        return type;
+    }
+    
+    public void setType(String type){
+        this.type = type;
+    }
 }
