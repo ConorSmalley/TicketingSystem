56ddd0cddabe888cfd966b6ae3721002fbc678e1
B2045995 <B2045995@9139-003H229697.hallam.shu.ac.uk>
2015-02-03 15:48:55 +0000

    Some work on inspection device

--- src/ticketing/system/InspectionDevice.java
+++ src/ticketing/system/InspectionDevice.java
@@ -1,28 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ticketing.system;
+
+import java.util.ArrayList;
 
 /**
  *
  */
-public class InspectionDevice 
-{
-    
+public class InspectionDevice {
+
     private int Id;
-    private UserAccount CurrentUserAccount;
-    private Employee CurrentEmployee;
-    private TravelPoint NextStop;
-    private Route AssignedRoute;
+    private UserAccount currentUserAccount;
+    private Employee currentEmployee;   //via GUI
+    private TravelPoint nextStop;   //via GUI
+    private Route assignedRoute;    //via GUI
     private DigitalReader reader;
-    private GUI InspecGUI;
-    
-    public void activateScanning(){}
-    
+    private GUI inspecGUI;
+
+    // untested
+    public void activateScanning() {
+        reader.initialize();
+    }
+
+    //incomplete untested
+    public Boolean inspectToken() {
+        return false;       //I suppose this is actually called by digital reader
+        //determines vlidity based of accesibly current info, not passed info
+    }
+
+    //incomplete untested   //User Account is a travelers account not a inspector
+    public String setCurrentUserAccount() {
+        return "Incomplete";
+    }
+
+    //incomplete untested
+    public Boolean anyValidTicketOrPass() {
+        return false;
+    }
+
+    //incomplete untested
+    public ArrayList<Pass> requestCurrentPasses() {
+        return new ArrayList<Pass>();
+    }
+
+    //incomplete untested
+    public UserAccount getCurrentUserAccount() {
+        return currentUserAccount;
+    }
+
+    //incomplete untested
+    public boolean generateIfringement() {
+        return false;
+    }
+
+    //incomplete untested
+    public void clearCurrentUser() {
+        currentUserAccount = null;
+    }
+
     // public int inspectionToken
-    
-    
-    
 }
