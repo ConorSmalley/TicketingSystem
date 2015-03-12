7f6732eadb046a35bea888f65a8d3585a174067f
B2045995 <B2045995@AC06-032E213534.hallam.shu.ac.uk>
2015-03-12 15:48:52 +0000

    Made fixes to JavaDocs

--- src/ticketing/system/PaymentHub.java
+++ src/ticketing/system/PaymentHub.java
@@ -7,13 +7,17 @@ import java.util.List;
 import static ticketing.system.TransactionType.*;
 
 /**
- * Present for vehicles and gateways to allow or prevent user travel.<p>
- * Is the software which is delegated to by gates through which users travel.<p>
- * Handles logic to determine whether users can travel through gates.<p>
+ * Present for vehicles and gateways to determine whether to allow or prevent user travel.
+ * Is the software which is delegated to by gates through which users travel.
+ * Handles logic to determine whether users can travel through gates.
  * Does not make changes to users account balance but does call methods which
  * charge the users account for travel if applicable.
+ * @author      Group C
+ * @version     1.0
+ * @since   04/03/2015
+ * @serial (or @serialField or @serialData)     This class does not make use of serialization.
  */
-class PaymentHub {
+public class PaymentHub {
 
     private Area area;     //this is the area location
     private TravelPoint parentTravel = null;
@@ -21,6 +25,11 @@ class PaymentHub {
     private int numGateways;
     private List<Gateway> gateways;
 
+    /**
+     * The constructor of PaymentHubs. Initialises vital member variables.
+     * @param   parent a TravelPoint or Vehicle for which the PaymentHub functions
+     * @param   numGateways a count of gateways to be initialised for this PaymentHub
+     */
     public PaymentHub(Object parent, int numGateways) {
         if (parent.getClass() == TravelPoint.class) {
             this.parentTravel = (TravelPoint) parent;
@@ -35,6 +44,11 @@ class PaymentHub {
         }
     }
 
+    /**
+     * Uses a token ID to identify a user who is attempting to travel and inspects their account to see if they can travel though this travelpoint.
+     * @param   tokenId the ID of a token to be searched for in the system.
+     * @return  A boolean value, true for able to travel, false for travel denied.
+     */
     public boolean canUserTravel(int tokenId) {
         Ticket ticket;
         UserAccount user;
@@ -71,29 +85,52 @@ class PaymentHub {
         }
     }
 
+    /**
+     * Applies a ticket to a user account.
+     * @param   ticket, the ticket to be matched to an account
+     * @param   token, the tokenID of a user who the ticket should be matched to.
+     */
     public void applyTicketToAcount(Ticket ticket, int token) {
         UserAccount user = UserAccountManager.getInstance().getUserAccountByTokenId(token);
         user.setActiveTicket(ticket);
     }
-
+    
+    /**
+     * Compares travel cost for this paymentHub and the balance available for a given user
+     * @param   user the ID of a token to be searched for in the system.
+     * @return  true in the event of user having account greater than the travel cost.
+     */
     private boolean canUserAffordPayment(UserAccount user) {
         System.out.println(user);
         System.out.println(getParentTravelPoint());
         System.out.println(getParentTravelPoint().getPrice());
         return user.canAccountBeDebited(getParentTravelPoint().getPrice());
     }
-
+    
+    /**
+     * Automatically charges a user an amount based on member variables of this PaymentHub object when given a user account.
+     * @param   user a UserAccount which will be charged.
+     */
     private void purchaseTicket(UserAccount user) {
         Transaction transaction = new Transaction(Cash, getParentTravelPoint().getPrice(), new Date());
         user.makePayment(getParentTravelPoint().getPrice());
         user.addTransaction(transaction);
     }
-
+    
+    /**
+     * Gets the location of this PaymentHub.
+     * @return  TravelPoint the parent location of this PaymentHub if applicable.
+     */
     private TravelPoint getParentTravelPoint() {
         return parentTravel != null ? parentTravel
                 : parentVehicle.getTravelPoint();
     }
-
+    
+    /**
+     * Determines whether the user currently has a valid pass or ticket allowing them to travel on the route on which this PaymentHub is situated and grants passage if they do.
+     * @param   tokenId the ID of a token which matches the user who is attempting to travel.
+     * @return  Boolean. True for passage allowed and false for passage disallowed. 
+     */
     public boolean canUserLeave(int tokenId) {
         //Acquires a user and retrieves current ticket
         UserAccount acct = (UserAccountManager.getInstance().getUserAccountByTokenId(tokenId));
@@ -105,11 +142,19 @@ class PaymentHub {
         startAutomatedPayment(acct);
         return true;
     }
-
+    
+    /**
+     * Take an area to be assigned to this PaymentHub.
+     * @param   a an area being passed in which is set to a member variable of the hub.
+     */
     public void setArea(Area a) {
         area = a;
     }
-
+    
+    /**
+     * Uses the given token ID to conduct a linear search for the first valid ticket owned by a user which would allow them to travel. When found clears this ticket which may incur an automatic charge. (See implementation of clear ticket)
+     * @param   acct the account to be charged for travel and to be searched for a ticket.
+     */
     void startAutomatedPayment(UserAccount acct) {
         List<Route> relevantRoutes = area.getRoutes();     //temp routes
         //tested, iterator methods works for purpose of function. No idea what the nested for stuff did but would always cause nullpointers.
@@ -132,11 +177,20 @@ class PaymentHub {
 //            }
 //        }
     }
-
+    
+    /**
+     * Uses a given gateway ID to search for a held gateway.
+     * @param   i the ID of a gateway to be searched for.
+     * @return  A Gateway reference which has the ID which was searched for.
+     */
     public Gateway getGatewayById(int i) {
         return gateways.get(i);
     }
-
+    
+    /**
+     * Gets an area which the PaymentHub is present in.
+     * @return  Area an area which this PayMentHub is Present in.
+     */
     public Area getArea() {
         return area;
     }
